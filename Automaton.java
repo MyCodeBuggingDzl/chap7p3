a5e11b4b647431be2b4693e56d77047c55e53276
Kian Denzel Ombao <kiandenzel@yahoo.com>
2025-10-24 09:33:28 -0400

    30 done, conditional operation

--- Automaton.java
+++ Automaton.java
@@ -50,21 +50,14 @@ public class Automaton
         int[] nextState = new int[state.length];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
+           
         for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
-            center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
+            int left = ( i==0 ) ? 0 : state[i-1];
+            int center = state[i];
+            int right = (i+1<state.length) ? state[i+1] : 0;
+                
+            right = state[i + 1];
+        
             nextState[i] = (left + center + right) % 2;
         }
         state = nextState;
