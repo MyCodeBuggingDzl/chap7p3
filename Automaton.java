dd50e4f80640702e21d6ce2c875a7bc1f3c4a740
Kian Denzel Ombao <kiandenzel@yahoo.com>
2025-10-24 11:09:50 -0400

    copied demanded code for 33

--- Automaton.java
+++ Automaton.java
@@ -47,7 +47,7 @@ public class Automaton
     public void update()
     {
         // Build the new state in a separate array.
-        
+        int[] nextState = new int[state.length];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
         int left = 0;
@@ -55,17 +55,21 @@ public class Automaton
         
         for(int i = 0; i < state.length; i++) {
             
-            int right = (i+1<state.length) ? state[i+1] : 0;
+            int right = i+1<state.length ? state[i+1] : 0;
                 
         
-            int newCell = (left + center + right) % 2;
+            nextState[i] = (left + center + right) % 2;
             left = center;
             center = right;
-            state[i] = newCell;
+           
         }
-        
+        state = nextState;
     }
     
+    public void calculateNextState()
+    {
+        
+    }
     /**
      * Reset the automaton.
      */
